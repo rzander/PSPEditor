c937d1725757f027aea96fc067906b2baf1fb5cb
Roger Zander <roger@zander.ch>
2021-09-07 10:33:25 +0200

    Update References and .NET version

--- PSPEdit/PSPEditLib/Properties/AssemblyInfo.cs
+++ PSPEdit/PSPEditLib/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Zander Tools")]
 [assembly: AssemblyProduct("PSPEditLib")]
-[assembly: AssemblyCopyright("Copyright ©  2019 by Roger Zander")]
+[assembly: AssemblyCopyright("Copyright ©  2021 by Roger Zander")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.4")]
+[assembly: AssemblyFileVersion("1.0.0.6")]
